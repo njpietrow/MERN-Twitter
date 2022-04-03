07c2bc1defb56c04b6c6bc118719031f7cc250c8
Nick Pietrow <njpietrow@gmail.com>
2022-04-03 11:12:24 -0500

    finished frontend tweets

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,10 +1,14 @@
 import { combineReducers } from "redux";
 import sessionReducer from "./session_reducer";
 import errorsReducer from "./errors_reducer";
+import sessionApiReducer from "./session_api_reducer";
+import TweetsReducer from "./tweets_reducer";
 
 const RootReducer = combineReducers({
-  session: sessionReducer,
-  errors: errorsReducer
+  // session: sessionReducer,
+  session: sessionApiReducer,
+  errors: errorsReducer,
+  tweets: TweetsReducer,
 });
 
 export default RootReducer;
